bf29a31e85059c51828963be029bfaea07c98171
M. Serdar Karaman <m.serdar.karaman@ya.ru>
2017-04-15 06:37:25 +0300

    +sayaç +güvenlik

--- Automatic_door.ino
+++ Automatic_door.ino
@@ -1,42 +1,25 @@
-//LCD 2x16
-//
-//
-//ısı+nem sensörünü ekrana yazdır
-//
-//if (kapı aç and kapı kapalı)
-//  switche kadar aç
-//    kapı açık
-//
-//
-//if (kapıyı kapat and kapı açık)
-//  araya engel gelirse kapıyı aç
-//    kapı kapıtıldı, lazeri kapat
-//
-//
-//if kapı açık
-//  sayım yap 0-1-0
-
 #include <Adafruit_Sensor.h>
 #include <LiquidCrystal.h>
 #include "DHT.h"
-#define DHTPIN 8
+
+#define DHTPIN 8 //nem-ısı sensörü
 #define Buzzer_pin 53 
-#define stepPin 51 
-#define dirPin 49
+#define stepPin 51 //step motor kontrol
+#define dirPin 49 //step motor yönü
 #define Pin_switch_open 47
-#define Pin_switch_close 45
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define Pin_switch_close 45 
+#define Pin_lazer 43  //lazeri aç kapa için
+#define Pin_sayac A0 //LDR okuma için
 #define DHTTYPE DHT11
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 DHT dht(DHTPIN, DHTTYPE);
 
-
-
-
-
-
 int deger=2;
-boolean kapi_acik = false;
-int lazer_engel;
+int sayac=0;
+int state=0;
+boolean kapi_durumu=false; //false:kapalı     true:açık
+boolean engel=false; //false:engele takılmadı     true:engel var.
 
 void setup() {
   pinMode(Buzzer_pin,OUTPUT);
@@ -44,7 +27,9 @@ void setup() {
   pinMode(dirPin,OUTPUT);
   pinMode(Pin_switch_open,INPUT);
   pinMode(Pin_switch_close,INPUT);
-  
+  pinMode(Pin_lazer,OUTPUT);
+  pinMode(Pin_sayac,INPUT);
+     
   Serial.begin(9600);
   Serial1.begin(9600);
   Serial.println("SETUP");
@@ -54,11 +39,13 @@ void setup() {
   lcd.print("S=");
   lcd.setCursor(9,0);
   lcd.print("N=");
+  lcd.setCursor(0,1);
+  lcd.print("SAYAC=0");
   
 }
 
 void loop() {
-  
+
   // Isı nem sensörünü oku ekrana yazdır.
   float h = dht.readHumidity();
   float t = dht.readTemperature();
@@ -77,15 +64,12 @@ void loop() {
       delay(500);
       digitalWrite(Buzzer_pin,LOW);
   }
-
-  
-  
   
   if (Serial1.available()){
+    engel=false;
     deger=Serial1.read();
-    Serial.println(deger);
     
-    if(deger=='1'){//Kapıyı aç
+    if( (deger=='1') && (kapi_durumu==false) ){//Kapıyı aç
       Serial.println("Kapi aciliyor");
       digitalWrite(dirPin,LOW);
       while (digitalRead(Pin_switch_open) == 1){//kapı tamamen açılana kadar
@@ -95,30 +79,79 @@ void loop() {
         delayMicroseconds(500); 
       }
       Serial.println("Kapi acildi");
-      
-      
+      digitalWrite(Pin_lazer,HIGH);
+      delayMicroseconds(1000);
+      kapi_durumu=true;
+    
     }//kapıyı aç
     
     
-    if(deger=='0'){//Kapıyı aç
+    if((deger=='0') && (kapi_durumu==true) ){//Kapıyı kapat
       Serial.println("Kapi kapaniyor");
       digitalWrite(dirPin,HIGH);
-      while (digitalRead(Pin_switch_close) == 1){//kapı tamamen açılana kadar
+      while (digitalRead(Pin_switch_close) == 1){//kapı tamamen kapanana kadar
+        if(analogRead(A0)>1024/2){
+          engel=true;
+          break;  
+        }
         digitalWrite(stepPin,HIGH); 
         delayMicroseconds(500); 
         digitalWrite(stepPin,LOW); 
         delayMicroseconds(500); 
       }
-      Serial.println("Kapi kapandi");
+      if(engel==true){
+        Serial.println("Kapi engele takıldı");
+        digitalWrite(dirPin,LOW);//motor yönünü ters çevir
+        while (digitalRead(Pin_switch_open) == 1){//kapı tamamen açılana kadar
+          digitalWrite(stepPin,HIGH); 
+          delayMicroseconds(500); 
+          digitalWrite(stepPin,LOW); 
+          delayMicroseconds(500); 
+        }
+        Serial.println("Kapi tekrar acildi");
+        digitalWrite(Pin_lazer,HIGH);
+        delayMicroseconds(1000);
+        engel==false;
+        
+      }
+      else{
+        Serial.println("Kapi kapandi");
+        digitalWrite(Pin_lazer,LOW);
+        delayMicroseconds(1000);
+        kapi_durumu=false;
+      }
+      
+      
+     }//kapıyı kapat
+     
     
-     }//kapıyı aç
+  }// serial avaiable kapat
+  
+  if (kapi_durumu==true){//kapı açık sayım yap
+  //0-1-0
     
-  }//
-  
-  
-  
+    if(analogRead(A0)>1024/2){
+      engel=true;
+    }
+    else{
+      engel=false;
+    }
+    if(state==0 && engel ==true){//0 -> 1
+      state=1;
+    }
+    else if(state==1 && engel ==false){//1 -> 0
+      state=2;
+      sayac=sayac+1;
+      lcd.setCursor(6, 1);
+      lcd.print(sayac);
+      state=0;
+    }
+    
+    
+  }  
   
 }//main loop kapat
+
 
 
 
